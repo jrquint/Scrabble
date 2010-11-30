721d77bcb08876035662480cbe8343224de1ed1a
Kelley van Evert <kelley.vanevert@gmail.com>
2010-11-30 20:34:25 +0100

    Completed initial work on javascript interface

--- scrabble.js
+++ scrabble.js
@@ -23,7 +23,7 @@ var ScrabbleGame = new Class(
 	{
 		this.el = $(el);
 		this._setupBoard();
-		this._setupNavigation();
+		this._setupActivities();
 	},
 	
 	/**
@@ -31,16 +31,6 @@ var ScrabbleGame = new Class(
 	 */
 	_setupBoard: function()
 	{
-		/*
-		// Input element
-		this.input = new Element('input', {
-			'id': 'in',
-			'type': 'text',
-		});
-		this.input.inject(this.el, 'bottom');
-		this.input.focus();
-		*/
-		
 		// Board
 		this.board = new Element('div', {
 			'id': 'board',
@@ -67,8 +57,10 @@ var ScrabbleGame = new Class(
 					'left': (3 + x*33).toString() + 'px',
 					'top': (3 + y*33).toString() + 'px',
 				});
-				field.store('x', x);
-				field.store('y', y);
+				field.store('pos', {
+					x: x,
+					y: y,
+				});
 				field.inject(this.board, 'bottom');
 				this.fields[x][y] = field;
 			}
@@ -76,124 +68,152 @@ var ScrabbleGame = new Class(
 	},
 	
 	/**
-	 * Sets up navigation variables and event handlers
+	 * Returns the field at given position
 	 */
-	_setupNavigation: function()
+	fieldAt: function(pos)
 	{
-		// Focus & position activity
-		this.at = {x:7, y:7};
-		this.focused = false;
+		return this.fields[pos.x][pos.y];
+	},
+	
+	/**
+	 * Sets up activity variables and event handlers
+	 */
+	_setupActivities: function()
+	{
+		/*
+			An activity denotes what level of interaction a user is in.
+			Activities fit a total order, therefore it is a kind of implicit stack.
+			The idea of "activities" is made concrete, because it denotes, for example, the
+			  amount of times the user has to press "esc" to lose focus etc from the board.
+			The activities are:
+				0. Sleep:      the user has no focus on the game board
+				1. Navigation: the user is navigating through the board, but has not written anything yet.
+				2. Write:      the user has written some letters (and can of course still navigate..).
+		*/
+		this.activities = {
+			Write:    2,
+			Navigate: 1,
+			Sleep:    0,
+		};
+		this.activity = 0;
 		
-		// Write word activity
-		this.writing = false;
+		// Position; initial position is center of board.
+		// Always set: even is activity is false, position is remembered
+		this.pos = {
+			x: 7,
+			y: 7,
+		};
+		
+		// Written letters; empty set if activity is lower than "write"
 		this.written = []; // Set of (letter-field)'s as an array
 		
-		// Mouse clicks
-		$$('.field').addEvent('click', (function(e)
-		{
-			// Use move method
-			this.move($(e.target).retrieve('x'), $(e.target).retrieve('y'));
+		// Mouse clicks can instantaniously move focus to new field
+		$$('.field').addEvent('click', (function(e) {
+			this.move($(e.target).retrieve('pos'));
 		}).bind(this));
 		
-		/*
-		// Letter input
-		this.input.addEvent('keydown', function(e)
-		{
-			if (e.code < 65 || e.code > 90) return false;
-		});
-		*/
-		
-		// Shortcuts & Letters
+		// Key events: navigation shortcuts & letter input
 		var opts = {
 			defaultEventType: 'keydown',
 			events: {
-				'up':    (function(e) { this.move( 0,-1, true); e.stop(); }).bind(this),
-				'down':  (function(e) { this.move( 0, 1, true); e.stop(); }).bind(this),
-				'right': (function(e) { this.move( 1, 0, true); e.stop(); }).bind(this),
-				'left':  (function(e) { this.move(-1, 0, true); e.stop(); }).bind(this),
-				'esc':   (function(e) { this.stop_activity();   e.stop(); }).bind(this),
+				'up':    (function(e) { this.move({x: 0, y:-1}, true); e.stop(); }).bind(this),
+				'down':  (function(e) { this.move({x: 0, y: 1}, true); e.stop(); }).bind(this),
+				'right': (function(e) { this.move({x: 1, y: 0}, true); e.stop(); }).bind(this),
+				'left':  (function(e) { this.move({x:-1, y: 0}, true); e.stop(); }).bind(this),
+				'esc':   (function(e) { this.drop_activity();   e.stop(); }).bind(this),
 			},
 		};
 		Array.each('abcdefghijklmnopqrstuvwxyz'.split(''), function(letter)
 		{
-			opts.events[letter] = (function(e) { this.input_letter(letter); e.stop(); }).bind(this);
+			opts.events[letter] = (function(e) { this.write_letter(letter); e.stop(); }).bind(this);
 		}, this);
 		this.keyboard_events = new Keyboard(opts);
 	},
 	
 	/**
-	 * Stops the current activity.
-	 * If writing a word --> remove word from board
-	 * If only focused   --> unfocus
+	 * Drops down an activity
 	 */
-	stop_activity: function()
+	drop_activity: function()
 	{
-		if (this.writing)
+		if (this.activity == this.activities.Navigate)
 		{
-			this.writing = false;
-			this.orientation = undefined;
-			this.written = {start: undefined, end: undefined};
+			this.remove_focus();
+			this.activity = this.activities.Sleep;
 		}
-		else if (this.focused)
+		else if (this.activity == this.activities.Write)
 		{
-			this.unfocus_field();
-			return true;
+			this.remove_letters();
+			this.activity = this.activities.Navigate;
 		}
 	},
 	
 	/**
-	 * Move the selected field to given (relative or absolute) coordinate,
-	 * OR IF NOT FOCUSED --> just focus
+	 * Move focused field to given (possibly relative) position.
+	 * For relative movement, pass along third, boolean true, argument.
+	 * If activity lower than Navigate --> activity is raised to Navigate.
 	 */
-	move: function(x, y)
+	move: function(newpos)
 	{
-		var relative = arguments[2] || false;
+		if (this.activity < this.activities.Navigate)
+		{
+			this.put_focus(this.pos);
+			this.activity = this.activities.Navigate;
+			return;
+		}
 		
-		var new_x = ((relative ? this.at.x + x : x) + 15) % 15;
-		var new_y = ((relative ? this.at.y + y : y) + 15) % 15;
+		var relative = arguments[1] || false;
 		
-		// Unfocus, change coordinate, refocus
-		this.unfocus_field();
-		this.at = {x: new_x, y: new_y};
-		this.focus_field();
+		newpos.x = ((relative ? this.pos.x + newpos.x : newpos.x) + 15) % 15;
+		newpos.y = ((relative ? this.pos.y + newpos.y : newpos.y) + 15) % 15;
+		
+		this.remove_focus();
+		this.pos = newpos;
+		this.put_focus();
 	},
 	
 	/**
-	 * Unfocus field at current position
+	 * Removes focus
 	 */
-	unfocus_field: function()
+	remove_focus: function()
 	{
-		if (!this.focused)
-			return true;
-		
-		var f = this.fields[this.at.x][this.at.y];
-		f.removeClass('at');
-		
-		this.focused = false;
+		this.fieldAt(this.pos).removeClass('at');
 	},
 	
 	/**
-	 * Focus field at current position
+	 * Puts focus
 	 */
-	focus_field: function()
+	put_focus: function()
 	{
-		if (this.focused)
-			return true;
-		
-		var f = this.fields[this.at.x][this.at.y];
-		f.addClass('at');
-		
-		this.focused = true;
+		this.fieldAt(this.pos).addClass('at');
 	},
 	
-	input_letter: function(letter)
+	/**
+	 * Remove all temporary letters from the board
+	 */
+	remove_letters: function()
 	{
-		if (!this.focused)
-			return true;
+		Array.each(this.written, function(field)
+		{
+			field.set('text', '').removeClass('written');
+		});
+		this.written = [];
+		return true;
+	},
+	
+	/**
+	 * Writes given letter to field at current position.
+	 * Activity is raised to Write.
+	 */
+	write_letter: function(letter)
+	{
+		// Activity checks
+		if (this.activity < this.activities.Navigate)
+		{
+			this.move(this.pos);
+		}
+		this.activity = Math.max(this.activity, this.activities.Write);
 		
-		this.writing = true;
-		
-		var f = this.fields[this.at.x][this.at.y];
-		f.set('text', letter);
+		// Write letter
+		this.written.push(this.fieldAt(this.pos).addClass('written').set('text', letter));
 	},
 });
